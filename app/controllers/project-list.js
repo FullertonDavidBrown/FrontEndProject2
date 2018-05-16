cc2ae76f04ac556eb80a61286639a54366bfeb2f
Jonathan-Vega1 <Jonathan-Vega1@users.noreply.github.com>
2018-05-15 22:38:41 -0700

    popularity button
    attempt to get the popularity button to work. Cannot pull the data in app/controller/project-list.js

--- app/controllers/project-list.js
+++ app/controllers/project-list.js
@@ -1,9 +1,25 @@
 import Controller from '@ember/controller';
 
+//var popRating = document.getElementById("popId");
+//var firebasePopularityRef = firebase.database().ref().child("popularity")
+
 export default Controller.extend({
   actions: {
     upVote(){
+
+      //var blogPost = this.get('store').findRecord('blog-post', 1); // => GET /blog-posts/1
+      //this.store.findAll('project');
+      var popUp = this.store.peekRecord('popularity',1); //output message, "Null"
+      //var popUp = this.store.findAll('project') //output a message, "<DS.PromiseArray:ember483>" the number after ember is a allocated value that has nothing to do with the databse from what i can tell.
+      alert(popUp); //this still comes up as null.
+      alert("upVote");
       this.incrementProperty('popularity');
-    }
+    },
+
+    downVote(){
+      alert("downVote")
+      //alert({{project.popularity}})
+      this.deincrementProperty('popularity');
+    },
   }
 });
