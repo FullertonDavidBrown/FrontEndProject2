f797c99e212a7ecbaaf2be50d3d24d3cd6a34260
Imran <32243275+imrangosla@users.noreply.github.com>
2018-05-16 03:58:16 -0700

    Some more tests

--- app/controllers/project-list.js
+++ app/controllers/project-list.js
@@ -2,8 +2,8 @@ import Controller from '@ember/controller';
 
 export default Controller.extend({
   actions: {
-    upVote(){
-      this.incrementProperty('popularity');
+    upVote() {
+ 
     }
   }
 });
